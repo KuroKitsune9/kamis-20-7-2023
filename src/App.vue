c079cc56ed60f9eadaf0fe783419bc029ea933cc
Muharafi Dalilah <muhapintar@gmail.com>
2023-07-25 09:36:13 +0700

    update ke 6

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div>
     <h1><u>Data diri</u></h1>
     <my-component nama="Muharafi Dalilah" umur="17" TTL="Jakarta 09-02-2006" Kelamin="Laki-Laki" agama="Islam" Alamat="Gg.Babakan TVRI No 29"></my-component>
-    
+    <Hobi hobi="Main Game"></Hobi>
   </div>
 </template>
 
 <script>
 import MyComponent from './MyComponent.vue';
+import Hobi from './Hobi.Vue';
 
 export default{
   components:{
-    MyComponent
+    MyComponent,
+    Hobi
   }
 }
 </script>
---
+++ src/Hobi.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Hobi kamu: {{ hobi }}</h2>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['hobi'],
+    }
+</script>
